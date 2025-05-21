81ce79edb4f6712fae5b9b42f432f762e41731ff
jazmy <jazmy@JAZ-LAPTOP>
2025-05-21 16:31:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int[] myGrades = { 85, 92, 76, 100, 79, 82, 94, 96 };
+
+Console.WriteLine("Original order: ");
+foreach (int grade in myGrades)
+{
+    Console.Write(grade + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < myGrades.Length - 1; i++)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < myGrades.Length; j++)
+    {
+        if (myGrades[j] < myGrades[minIndex])
+        {
+            minIndex = j;
+        }
+    }
+    int temp = myGrades[minIndex];
+    myGrades[minIndex] = myGrades[i];
+    myGrades[i] = temp;
+}
+
+Console.WriteLine("\nNew order:");
+foreach (int grade in myGrades)
+{
+    Console.Write(grade + " ");
+}
+
+Console.ReadKey();
+
+
+
